a273b21f950d924bc2a552070ce0b78c11855143
Alex Malkov <malkov.fighter@yandex.ru>
2024-04-13 17:20:01 +0700

    fix webvisor 01

--- src/blocks/MainContent/MainContent.tsx
+++ src/blocks/MainContent/MainContent.tsx
@@ -1,5 +1,8 @@
 import { Container } from './MainContent.styled';
 
 export const MainContent = () => {
+
+    console.log("Test webvisor 01");
+    
     return <Container>Test webvisor</Container>
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { StyleSheetManager } from "styled-components";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <StyleSheetManager disableCSSOMInjection><Component {...pageProps} /></StyleSheetManager>;
 }
