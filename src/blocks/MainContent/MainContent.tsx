3d35c95c350ea96c3508b937145262846ba29cd4
Alex Malkov <malkov.fighter@yandex.ru>
2024-04-16 04:34:49 +0700

    10

--- src/blocks/MainContent/MainContent.tsx
+++ src/blocks/MainContent/MainContent.tsx
@@ -2,7 +2,7 @@ import { Container } from './MainContent.styled';
 
 export const MainContent = () => {
 
-    console.log("Test webvisor 09");
+    console.log("Test webvisor 10");
     
-    return <Container>Test webvisor 09</Container>
+    return <Container>Test webvisor 10</Container>
 }
