c1f3058a234e95fe97c64d9f28682d0771705372
Alex Malkov <malkov.fighter@yandex.ru>
2024-04-16 04:27:17 +0700

    09

--- src/blocks/MainContent/MainContent.tsx
+++ src/blocks/MainContent/MainContent.tsx
@@ -2,7 +2,7 @@ import { Container } from './MainContent.styled';
 
 export const MainContent = () => {
 
-    console.log("Test webvisor 08");
+    console.log("Test webvisor 09");
     
-    return <Container>Test webvisor 08</Container>
+    return <Container>Test webvisor 09</Container>
 }
--- src/pages/registry.tsx
+++ src/pages/registry.tsx
@@ -1,27 +1,32 @@
 'use client'
- 
+
 import React, { useState } from 'react'
 import { useServerInsertedHTML } from 'next/navigation'
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
- 
+
 export default function StyledComponentsRegistry({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet())
- 
-  useServerInsertedHTML(() => {
-    const styles = styledComponentsStyleSheet.getStyleElement()
-    styledComponentsStyleSheet.instance.clearTag()
-    return <>{styles}</>
-  })
- 
-  if (typeof window !== 'undefined') return <>{children}</>
- 
-  return (
-    <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
-      {children}
-    </StyleSheetManager>
-  )
+    const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet())
+
+    useServerInsertedHTML(() => {
+        const styles = styledComponentsStyleSheet.getStyleElement()
+        styledComponentsStyleSheet.instance.clearTag()
+        console.log(styles);
+        return <>{styles}</>
+    })
+
+    if (typeof window !== 'undefined') return (
+        <StyleSheetManager disableCSSOMInjection>
+            {children}
+        </StyleSheetManager>
+    );
+
+    return (
+        <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
+            {children}
+        </StyleSheetManager>
+    );
 }
