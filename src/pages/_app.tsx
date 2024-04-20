0ccf8d9071c1aaab6b178f912be0d74dcedd1cd1
Alex Malkov <malkov.fighter@yandex.ru>
2024-04-20 13:48:02 +0700

    createGlobalStyle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { createGlobalStyle } from 'styled-components'
+
+import { DropdownMenu } from '../blocks/MainMenu/MainMenu.styled';
+
+const GlobalStyle = createGlobalStyle`
+  .${DropdownMenu.styledComponentId} {
+    ${DropdownMenu.componentStyle.rules}
+  }
+`
+
 export default function App({ Component, pageProps }: AppProps) {
+  console.log(DropdownMenu);
   return (
-    <Component {...pageProps} />
+    <>
+
+      <GlobalStyle />
+
+      <Component {...pageProps} />
+
+    </>
   );
 }
